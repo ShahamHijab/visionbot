750848735b046de51a5bbd0d0255743c55b78a0e
[ShahamHijab] <shaham434789@gmail.com>
2025-12-25 23:52:15 +0500

    minor changes

--- mad_project/lib/main.dart
+++ mad_project/lib/main.dart
@@ -28,28 +28,3 @@ class VisionBotApp extends StatelessWidget {
     );
   }
 }
-
-
-
-// import 'package:flutter/material.dart';
-// import 'routes/app_routes.dart';
-// import 'theme/app_theme.dart';
-
-// void main() {
-//   runApp(const VisionBotApp());
-// }
-
-// class VisionBotApp extends StatelessWidget {
-//   const VisionBotApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Vision Bot',
-//       debugShowCheckedModeBanner: false,
-//       theme: AppTheme.lightTheme,
-//       initialRoute: AppRoutes.splash,
-//       routes: AppRoutes.routes,
-//     );
-//   }
-// }
